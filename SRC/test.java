442e2cdf1af013ababebdd99cb001f66cc0742f9
agitaltintas <agitaltintas6565@gmail.com>
2022-12-27 18:15:35 -0800

    Another line

--- SRC/test.java
+++ SRC/test.java
@@ -4,18 +4,18 @@ I added this line from GitHub.
 
 Different line
 
-        This is another line come from another branch.
+This is another line come from another branch.
 
-        commit and push -> When you change something from your laptop,
-        to send them to GitHub(Server), you will use commit and push button.
+commit and push -> When you change something from your laptop,
+to send them to GitHub(Server), you will use commit and push button.
 
-        commit -> save my changes to my local repo
-        push   -> sending your committed changes to github(Server)
+commit -> save my changes to my local repo
+push   -> sending your committed changes to github(Server)
 
 
-        if you want to get the latest code from server
-        Git -> pull
+if you want to get the latest code from server
+Git -> pull
 
-        pull -> pull the latest changes from the Github(Server)
+pull -> pull the latest changes from the Github(Server)
 
 
